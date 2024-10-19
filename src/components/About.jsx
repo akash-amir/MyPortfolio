93f0dfb67ea91a2a67c0b8c6c424a7fde7807406
akash-amir <akashamir537@gmail.com>
2024-10-19 13:59:13 +0500

    Your commit message here

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ return(
     <>
     <h1 id='About' className="heading">About Me</h1>
     <div className='about'>
-    <p > Hi! I'm [Your Name], a passionate and creative web developer with a strong focus on delivering high-quality, responsive websites. With a background in creating seamless user experiences, I specialize in designing and building functional, intuitive websites for small businesses, especially in the GYM and fitness industry.</p>
+    <p > Hi! I'm Akash , a passionate and creative web developer with a strong focus on delivering high-quality, responsive websites. With a background in creating seamless user experiences, I specialize in designing and building functional, intuitive websites for small businesses, especially in the GYM and fitness industry.</p>
  </div>
     </>
 )
